38f2d481f8f9f120188ad45df3e150defedc2b8a
omar-muhammad <omarelsayedmuhammad@gmail.com>
2024-05-08 07:06:51 +0300

    fixed some props to avoid Styled Component error

--- src/ui/ConfirmDelete.jsx
+++ src/ui/ConfirmDelete.jsx
@@ -32,12 +32,12 @@ function ConfirmDelete({ resourceName, onConfirm, disabled, onCloseModal }) {
       <div>
         <Button
           onClick={onCloseModal}
-          variation='secondary'
+          $variation='secondary'
           disabled={disabled}
         >
           Cancel
         </Button>
-        <Button onClick={onConfirm} variation='danger' disabled={disabled}>
+        <Button onClick={onConfirm} $variation='danger' disabled={disabled}>
           Delete
         </Button>
       </div>
