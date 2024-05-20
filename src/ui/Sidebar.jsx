92f3d5e56d23593380e081f53e7fa1d023affbd0
omar-muhammad <omarelsayedmuhammad@gmail.com>
2024-05-20 15:44:32 +0300

    removed unnecessary line of code

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 import Logo from './Logo.jsx';
 import MainNav from './MainNav.jsx';
-import Uploader from '../data/Uploader.jsx';
 
 const StyledAside = styled.aside`
   background-color: var(--color-grey-0);
